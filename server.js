2f658179edbad4a503cff2ec43f325aaad4b5028
wxsday <wxsday@gmail.com>
2020-05-29 15:02:02 -0400

    server fixes.

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const ex = require('express');
+const { wxo1, wxo2 } = require('./mid.js');
 
 const app = ex();
+// any port
 const port = 8000;
+// disable header
 app.disable('x-powered-by');
 
+//any response message
 const mess = { messages: 'public' };
 
-app.get('/', (req, res) => {
-  console.log(req.headers);
+app.use(wxo1);
+app.get('/', wxo2, (req, res) => {
+  //console.log(req.headers);
   res.send(mess);
 });
 
@@ -18,4 +23,4 @@ app.listen(port, () =>
 // some variable passsing from cmdln
 let args = process.argv;
 args.splice(0, 2);
-console.log(args);
+console.log('arguments passed from command->', args);
