0448b76b8b6cace3060affaf540492da755d2f01
wxsday <wxsday@gmail.com>
2020-05-29 14:03:47 -0400

    variable pass

--- server.js
+++ server.js
@@ -14,3 +14,8 @@ app.get('/', (req, res) => {
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
 );
+
+// some variable passsing from cmdln
+let args = process.argv;
+args.splice(0, 2);
+console.log(args);
